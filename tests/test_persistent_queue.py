780fae851f74279f9f4af63d5a5f9ab57a35a143
Philip Lundrigan <philiplundrigan@gmail.com>
2016-09-02 09:18:08 -0600

    Use real file system instead of test one
    It was causing problems now that I am syncing flushes to the file
    system.

--- tests/test_persistent_queue.py
+++ tests/test_persistent_queue.py
@@ -1,125 +1,124 @@
+import os
 import unittest
-
-from pyfakefs import fake_filesystem_unittest
+import uuid
 
 from persistent_queue import PersistentQueue
 
 
-class TestPersistentQueue(fake_filesystem_unittest.TestCase):
+class TestPersistentQueue(unittest.TestCase):
     def setUp(self):
-        self.setUpPyfakefs()
+        random = str(uuid.uuid4()).replace('-', '')
+        self.filename = '{}_{}'.format(self.id(), random)
+        self.queue = PersistentQueue(self.filename)
+
+    def tearDown(self):
+        os.remove(self.filename)
 
     def test_count(self):
-        queue = PersistentQueue()
-        self.assertEqual(len(queue), 0)
-        self.assertEqual(queue.count(), 0)
 
-        queue.push(1)
-        self.assertEqual(len(queue), 1)
-        self.assertEqual(queue.count(), 1)
+        self.assertEqual(len(self.queue), 0)
+        self.assertEqual(self.queue.count(), 0)
 
-        queue.push(2)
-        self.assertEqual(len(queue), 2)
-        self.assertEqual(queue.count(), 2)
+        self.queue.push(1)
+        self.assertEqual(len(self.queue), 1)
+        self.assertEqual(self.queue.count(), 1)
+
+        self.queue.push(2)
+        self.assertEqual(len(self.queue), 2)
+        self.assertEqual(self.queue.count(), 2)
 
         for i in range(100 + 1):
-            queue.push(i)
+            self.queue.push(i)
 
-        self.assertEqual(len(queue), 103)
-        self.assertEqual(queue.count(), 103)
+        self.assertEqual(len(self.queue), 103)
+        self.assertEqual(self.queue.count(), 103)
 
     def test_clear(self):
-        queue = PersistentQueue()
-        queue.push(5)
-        queue.push(50)
+        self.queue.push(5)
+        self.queue.push(50)
 
-        self.assertEqual(queue.peek(2), [5, 50])
-        self.assertEqual(len(queue), 2)
-        queue.clear()
-        self.assertEqual(len(queue), 0)
+        self.assertEqual(self.queue.peek(2), [5, 50])
+        self.assertEqual(len(self.queue), 2)
+        self.queue.clear()
+        self.assertEqual(len(self.queue), 0)
 
     def test_push(self):
-        queue = PersistentQueue()
-        queue.push(5)
-        self.assertEqual(queue.peek(1), 5)
+        self.queue.push(5)
+        self.assertEqual(self.queue.peek(1), 5)
 
-        queue.push([10, 15, 20])
-        self.assertEqual(queue.peek(4), [5, 10, 15, 20])
+        self.queue.push([10, 15, 20])
+        self.assertEqual(self.queue.peek(4), [5, 10, 15, 20])
 
         data = {"a": 1, "b": 2, "c": [1, 2, 3]}
-        queue.push(data)
-        self.assertEqual(queue.peek(5), [5, 10, 15, 20, data])
+        self.queue.push(data)
+        self.assertEqual(self.queue.peek(5), [5, 10, 15, 20, data])
 
     def test_pop(self):
-        queue = PersistentQueue()
-        queue.push('a')
-        queue.push('b')
-        self.assertEqual(len(queue), 2)
+        self.queue.push('a')
+        self.queue.push('b')
+        self.assertEqual(len(self.queue), 2)
 
-        self.assertEqual(queue.pop(), 'a')
-        self.assertEqual(len(queue), 1)
+        self.assertEqual(self.queue.pop(), 'a')
+        self.assertEqual(len(self.queue), 1)
 
-        self.assertEqual(queue.pop(1), 'b')
-        self.assertEqual(len(queue), 0)
+        self.assertEqual(self.queue.pop(1), 'b')
+        self.assertEqual(len(self.queue), 0)
 
-        queue.push('a')
-        queue.push('b')
-        queue.push('c')
-        queue.push('d')
-        self.assertEqual(len(queue), 4)
+        self.queue.push('a')
+        self.queue.push('b')
+        self.queue.push('c')
+        self.queue.push('d')
+        self.assertEqual(len(self.queue), 4)
 
-        self.assertEqual(queue.pop(3), ['a', 'b', 'c'])
-        self.assertEqual(len(queue), 1)
+        self.assertEqual(self.queue.pop(3), ['a', 'b', 'c'])
+        self.assertEqual(len(self.queue), 1)
 
-        self.assertEqual(queue.pop(100), ['d'])
-        self.assertEqual(len(queue), 0)
+        self.assertEqual(self.queue.pop(100), ['d'])
+        self.assertEqual(len(self.queue), 0)
 
-        self.assertEqual(queue.pop(100), [])
-        self.assertEqual(queue.pop(), None)
+        self.assertEqual(self.queue.pop(100), [])
+        self.assertEqual(self.queue.pop(), None)
 
     def test_peek(self):
-        queue = PersistentQueue()
-        queue.push(1)
-        queue.push(2)
-        queue.push("test")
+        self.queue.push(1)
+        self.queue.push(2)
+        self.queue.push("test")
 
-        self.assertEqual(queue.peek(), 1)
-        self.assertEqual(queue.peek(1), 1)
-        self.assertEqual(queue.peek(2), [1, 2])
-        self.assertEqual(queue.peek(3), [1, 2, "test"])
+        self.assertEqual(self.queue.peek(), 1)
+        self.assertEqual(self.queue.peek(1), 1)
+        self.assertEqual(self.queue.peek(2), [1, 2])
+        self.assertEqual(self.queue.peek(3), [1, 2, "test"])
 
-        self.assertEqual(queue.peek(100), [1, 2, "test"])
+        self.assertEqual(self.queue.peek(100), [1, 2, "test"])
 
-        queue.clear()
+        self.queue.clear()
 
-        queue.push(1)
-        self.assertEqual(len(queue), 1)
-        self.assertEqual(queue.peek(), 1)
-        self.assertEqual(queue.peek(1), 1)
-        self.assertEqual(queue.peek(2), [1])
+        self.queue.push(1)
+        self.assertEqual(len(self.queue), 1)
+        self.assertEqual(self.queue.peek(), 1)
+        self.assertEqual(self.queue.peek(1), 1)
+        self.assertEqual(self.queue.peek(2), [1])
 
     def test_big_file(self):
-        queue = PersistentQueue()
         data = {"a": list(range(1000))}
 
         for i in range(2000):
-            queue.push(data)
+            self.queue.push(data)
 
         for i in range(1995):
-            self.assertEqual(queue.pop(), data)
-            queue.flush()
+            self.assertEqual(self.queue.pop(), data)
+            self.queue.flush()
 
-        self.assertEqual(len(queue), 5)
+        self.assertEqual(len(self.queue), 5)
 
     def test_big_file_2(self):
-        queue = PersistentQueue()
         data = {"a": list(range(1000))}
 
         for i in range(2000):
-            queue.push(data)
+            self.queue.push(data)
 
-        self.assertEqual(queue.pop(1995), [data for i in range(1995)])
-        self.assertEqual(len(queue), 5)
+        self.assertEqual(self.queue.pop(1995), [data for i in range(1995)])
+        self.assertEqual(len(self.queue), 5)
 
 if __name__ == '__main__':
     unittest.main()
